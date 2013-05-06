7ef4e11fa5f38b80db9f20615e4ca5bdfe11d4a1
nneiros <nneiros@localhost>
2013-05-07 00:09:13 +0300

    ektypono th lista gia na vro thn thesi tou kathe pragmatos

--- python33/listes/lista.py
+++ python33/listes/lista.py
@@ -1,63 +1,44 @@
 #Python Εύρεση τιμών από list,μέσα σε κείμενο
+#lista
+kreas= ['Χοιρινο','Κοτοπουλο']
+piato= ['Δευτερο','Αλλο']
+garnitoura= ['Πατατες', 'Ρυζι','Αλλο']
+
 def check_k(lista,keimeno):
     i=0
-    if i <len (kreas):
-        if  kreas[i] in keimeno:
-            return True
-        else:
-            return False
-        i+=1
-        if i<len(piato):
-            if piato[i] in keimeno:
-                #i+=1
-                return True
-            else:
-                return False
-            i+=1
-            if i<len(garnitoura):
-                if garnitoura[i] in keimeno:
-                    #i+=1
-                    return True
-                else:
-                    return False
-                                        #return True
-
-                                    
-                                    #else:
-                                        #return False
-                                
-
-                                #else:
-                                    #return True
-                                
-                            #else:
-                                #return False
-                        #else:
-                            #return True
-                        
-                    #else:
-                        #return False
-
-                #else:
-                    #return True
-
-kreas= ['Πατατες', 'Ρυζι','Αλλο']
-piato= ['Δευτερο','Αλλο']
-garnitoura= ['Χοιρινο','Κοτοπουλο']
+    if i <len (lista):
+        if  lista[i] in keimeno:
+           i+=1
+           if i<len(lista):
+                if lista[i] in keimeno:
+                    i+=1
+                    if i<len(lista):
+                        if lista[i] in keimeno:
+                            return True
+                        else:
+                            return False
+                    else:
+                        return True
+                else:                                             
+                  return False       
+        else:                                             
+            return True                 
+                                  
 file ='lista2.txt'#->Διεύθυνση αρχείου  (Σαν dir_sin,έχω ορίσει πιο πριν το φάκελο του αρχείου,και σαν arxeio,την ονομασία του)
 f=open(file, "r", encoding="utf8")
 keimeno=f.read()
-#print (kreas,piato,garnitoura)
-print(keimeno)
 f.close()
 a=keimeno.split("\n")
-#print(a)
-gar=check_k(garnitoura, a[-3])
-pia=check_k(piato, a[-3])#???????????-2
-kre=check_k(kreas, a[-3])#??????????-1
-#print(garnitoura,a[-3])
-#print (piato,a[-2])
-#print (kreas,a[-1])
+print(keimeno)
+#print(kreas)
+#print(piato)
+#print(garnitoura)
+gar=check_k(garnitoura, a[-1])
+pia=check_k(piato, a[-2])
+kre=check_k(kreas, a[-3])
+#print (a[-5])#πρωτα κανεις εκτυπωση του a για να δεις την θεση του καθενος(piato κ,λ,π)
+#print (a[-4])
+#print (a[-3])
 print("garnitoura = ", gar)
 print("piato=",pia)
 print("kreas=",kre)
