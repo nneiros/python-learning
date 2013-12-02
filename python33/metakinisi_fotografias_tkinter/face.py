671f1ce00fc311bb7eaee0be2ad0592be9e54fe4
nneiros <38861064+nneiros@users.noreply.github.com>
2013-12-02 19:59:56 +0200

    egine onomasia parathyrou se tkinter

--- python33/metakinisi_fotografias_tkinter/face.py
+++ python33/metakinisi_fotografias_tkinter/face.py
@@ -1,11 +1,12 @@
 import time
 from tkinter import *
 tk = Tk()
+tk.title('Mετακινηση φωτογραφιας')
 canvas = Canvas(tk, width=400, height=400)
 canvas.pack()
 myimage = PhotoImage(file='face.gif')
 canvas.create_image(0, 0, anchor=NW, image=myimage)
-for x in range(0, 35):
+for x in range(0, 10):
     canvas.move(1, 10, 10)
     tk.update()
     time.sleep(0.1)#0.05
