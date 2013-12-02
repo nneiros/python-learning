a11a4a4e4d3682feb3d04c55624b92d778a8a8de
nneiros <nneiros@localhost>
2013-12-02 19:59:56 +0200

    egine onomasia parathyrou se tkinter

--- python33/metakinisi_fotografias_tkinter/shmaia_import.py
+++ python33/metakinisi_fotografias_tkinter/shmaia_import.py
@@ -1,8 +1,11 @@
 from tkinter import *
 tk = Tk()
+tk.title('Σημαιες')
 canvas = Canvas(tk, width=400, height=400)
+
 canvas.pack()
+
 myimage = PhotoImage(file="shmaia.gif")
-canvas.create_image(0, 0, image=myimage, anchor=NW)
+canvas.create_image(90, 90, image=myimage, anchor=NW)
 import os
 print(os.getcwd())
